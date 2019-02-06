32d6e1d3675fac83b83815cd9d9f4fcc48da70c2
MichalNogaGD <michalnoga8@gmail.com>
2019-02-06 11:23:50 +0100

    Stworzenie Snake

---
+++ SnakeGame/SnakeGame/MySnake.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace SnakeGame
+{
+    class MySnake
+    {
+        public SnakePart Head { get; private set; }
+        public List<SnakePart> Parts { get; private set; }
+
+        public MySnake()
+        {
+            Head = new SnakePart(20, 0);
+            Head.Rect.Width = Head.Rect.Height = 10;
+            Head.Rect.Fill = System.Windows.Media.Brushes.Red;
+            Parts = new List<SnakePart>();
+            Parts.Add(new SnakePart(19, 0));
+            Parts.Add(new SnakePart(18, 0));
+            Parts.Add(new SnakePart(17, 0));
+            Parts.Add(new SnakePart(16, 0));
+            Parts.Add(new SnakePart(15, 0));
+            Parts.Add(new SnakePart(14, 0));
+            Parts.Add(new SnakePart(13, 0));
+            Parts.Add(new SnakePart(12, 0));
+            Parts.Add(new SnakePart(11, 0));
+            Parts.Add(new SnakePart(10, 0));
+        }
+
+    }
+}
